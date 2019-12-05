2220c50ece22d26595b881a36ee449cf76beb5fe
Umberto Valente <uvalente89@gmail.com>
2019-12-05 10:47:42 +0000

    Creation of students array and updated puts method

--- directory.rb
+++ directory.rb
@@ -1,16 +1,20 @@
-student_count = 11
+# frozen_string_literal: true
 
-puts "The students of Villain Academy"
-puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex Delarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-puts "Overall we have #{student_count} great students."
+students = [
+  'Dr. Hannibal Lecter',
+  'Darth Vader',
+  'Nurse Ratched',
+  'Michael Corleone',
+  'Alex Delarge',
+  'The Wicked Witch of the West',
+  'Terminator',
+  'Freddy Krueger',
+  'The Joker',
+  'Joffrey Baratheon',
+  'Norman Bates'
+]
+
+puts 'The students of Villain Academy'
+puts '-------------'
+students.each { |student| puts student }
+puts "Overall we have #{students.count} great students."
