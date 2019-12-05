94fde0c57185eb7cb7c16b0a92253fedeb5fafb2
Umberto Valente <uvalente89@gmail.com>
2019-12-05 15:27:21 +0000

    add auto load students file

--- directory.rb
+++ directory.rb
@@ -6,11 +6,11 @@ def input_students
   puts 'Please enter the names of the students'
   puts 'To finish, just hit return twice'
 
-  name = gets.chomp
+  name = STDIN.gets.chomp
   until name.empty?
     @students << { name: name, cohort: :november }
     puts "Now we have #{@students.size} students"
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 
@@ -61,9 +61,10 @@ def process(selection)
 end
 
 def interactive_menu
+  try_load_students
   loop do
     print_menu
-    process(gets.chomp) 
+    process(STDIN.gets.chomp) 
   end
 end
 
@@ -78,13 +79,25 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(",")
     @students << { name: name, cohort: cohort.to_sym }
   end
   file.close
+end
+
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.size} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
 end
 
 interactive_menu
